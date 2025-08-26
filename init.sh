39819d66f10fe243a102cf67925d566f609c1c85
steveleo@basic.digitalocean <stevexfliu@gmail.com>
2025-08-26 14:15:38 +0000

    Starship and its configurations

--- init.sh
+++ init.sh
@@ -7,14 +7,14 @@ apt update
 apt upgrade -y
 
 # Install system tools
-apt install git wget
+apt install git curl
 
 # Install and change to zsh
 apt install zsh
 chsh -s $(which zsh)
 
 # Install oh-my-zh and plugins
-sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
@@ -25,3 +25,10 @@ git clone https://github.com/hlissner/zsh-autopair ${ZSH_CUSTOM:-~/.oh-my-zsh/cu
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 sed -i 's/plugins=(git)/plugins=(git zsh-autopair zsh-autosuggestions zsh=syntax-highlighting fast-syntax-highlighting)' "~/.zshrc"
+
+# Install and configure  Starship
+curl -sS https://starship.rs/install.sh | sh
+echo 'eval "$(starship init zsh)"' >> ~/.zshrc
+
+mkdir -p ~/.config && touch ~/.config/starship.toml
+starship preset no-runtime-versions -o ~/.config/starship.toml
